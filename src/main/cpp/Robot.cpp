f74a148cd087ba36fb6fae2075b4b55dcf95bc47
Money <xxmoneyman8902xx@gmail.com>
2019-01-16 14:34:20 -0600

    Cleaned out classes and Robot.cpp/Robot.cpp
    Edited:
    Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -6,16 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
-#include "RobotPeriodic.h"
-#include "RobotAutonomous.h"
-#include "RobotTeleop.h"
 #include "DriveBase.h"
 #include "Elevator.h"
 
 
 // Objects and variable for this file only
-RobotTeleop       * ControlTeleop;
-RobotAutonomous   * ControlAutonomous;
 DriveBase         * MecDrive;
 Elevator          * ControlElevator;
 
@@ -23,13 +18,8 @@ Elevator          * ControlElevator;
 #include <frc/smartdashboard/SmartDashboard.h>
 
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
   MecDrive          = new DriveBase(this);
-  ControlTeleop     = new RobotTeleop(this);
-  ControlAutonomous = new RobotAutonomous(this);
   ControlElevator   = new Elevator(this);
   
   pNavGyro.Init();
@@ -37,49 +27,41 @@ void Robot::RobotInit() {
 
 // ----------------------------------------------------------------------------
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
+void Robot::RobotPeriodic() 
+{
+
+}
+
+void Robot::AutonomousInit() 
+{
  
 }
 
-void Robot::AutonomousPeriodic() {
+void Robot::AutonomousPeriodic() 
+{
 }
 
-void Robot::TeleopInit() {}
+void Robot::TeleopInit() 
+{
+
+}
 
 // ----------------------------------------------------------------------------
 
 void Robot::TeleopPeriodic() 
   {
-    
+  //Teleop Functions
     MecDrive->Drive();
-    ControlTeleop->Periodic();  
     ControlElevator->Output();
   }
 
 // ============================================================================
 
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() 
+{
+
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
