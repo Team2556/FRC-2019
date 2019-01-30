935fd1fab1ac67ae239182b36d0acaf4e1d60781
bbaggerman <bob.git@baggerman.org>
2019-01-30 12:34:45 -0600

    Added definitions for DIO channels

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -2,7 +2,6 @@
 #define         XBOX_ONE                0
 #define         XBOX_TWO                1
 
-
 // CAN BUS IDs
 #define         CAN_PDP                 0
 #define         CAN_TALON_LF            1
@@ -14,3 +13,15 @@
 #define         CAN_TALON_LEFT_ELEV     7
 #define         CAN_TALON_RIGHT_ELEV    8
 #define         CAN_PCM                11
+
+// DIO Channels
+#define         DIO_LT_FRONT_1          0
+#define         DIO_LT_FRONT_2          1
+#define         DIO_LT_FRONT_3          2
+#define         DIO_LT_FRONT_4          3
+#define         DIO_LT_FRONT_5          4
+#define         DIO_LT_FRONT_6          5
+#define         DIO_LT_BACK_1           6
+#define         DIO_LT_BACK_2           7
+#define         DIO_US_RANGE_TRIGGER    8
+#define         DIO_US_RANGE_PULSE      9
