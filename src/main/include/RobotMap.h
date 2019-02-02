eed50a2166a3c0a2d6805ffc8aafbaff1bdad7a3
Matthew-VandenBoom <Bookster99@outlook.com>
2019-02-02 11:54:20 -0600

    Changes to robot map to allow for NavX DIOs

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -15,6 +15,10 @@
 #define         CAN_PCM                11
 
 // DIO Channels
+
+#define         NAVX_DIO
+
+#ifdef RIO_DIO // default
 #define         DIO_LT_FRONT_1          0
 #define         DIO_LT_FRONT_2          1
 #define         DIO_LT_FRONT_3          2
@@ -22,6 +26,23 @@
 #define         DIO_LT_FRONT_5          4
 #define         DIO_LT_FRONT_6          5
 #define         DIO_LT_BACK_1           6
-#define         DIO_LT_BACK_2           7
-#define         DIO_US_RANGE_TRIGGER    8
-#define         DIO_US_RANGE_PULSE      9
+#define         DIO_LT_BACK_2           
+#endif
+
+
+
+#ifdef NAVX_DIO // navx
+#define         DIO_LT_FRONT_1          10
+#define         DIO_LT_FRONT_2          11
+#define         DIO_LT_FRONT_3          12
+#define         DIO_LT_FRONT_4          13
+#define         DIO_LT_FRONT_5          18
+#define         DIO_LT_FRONT_6          19
+#define         DIO_LT_BACK_1           20
+#define         DIO_LT_BACK_2           21
+#endif
+
+#define         DIO_US_RANGE_TRIGGER        8
+#define         DIO_US_RANGE_PULSE          9
+#define         DIO_US_RANGE_TRIGGER_LEFT   6
+#define         DIO_US_RANGE_PULSE_LEFT     7
